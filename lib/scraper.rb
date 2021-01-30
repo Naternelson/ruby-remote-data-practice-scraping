0c6b883e362d79276f0fa2a5145f96a1f08c9f31
Nate <7nater.nelson@gmail.com>
2021-01-30 09:58:29 -0700

    commit tree 1c619cd61dc19a5e7c1cb59d32e2f3d6903d238f

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
+# #open is a method of the 'open-uri' module
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+thing = doc.css(".inner-3LPBeK .content-3OTK_R .text-4GLMvr .title-oE5vT4")
+puts thing
